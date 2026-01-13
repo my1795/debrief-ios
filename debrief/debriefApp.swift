b63e495bce4b3f4c69540d30c08daceef42e8f6f
my1795 <myldrm1025@gmail.com>
2026-01-13 14:22:54 +0400

    Initial Commit

---
+++ debrief/debriefApp.swift
@@ -0,0 +1,32 @@
+//
+//  debriefApp.swift
+//  debrief
+//
+//  Created by Mustafa Yıldırım on 13/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct debriefApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
